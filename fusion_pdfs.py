dbf249107d96f587625387d5a1872f6bcc285959
Herny664 <hern4n.roch0n@gmail.com>
2024-03-04 15:46:43 -0300

    Final changes

--- fusion_pdfs.py
+++ fusion_pdfs.py
@@ -1,20 +1,21 @@
 import streamlit as st
-import PyPDF
+import PyPDF2
 
 #Vars
 output_pdf = "documents/pdf_combined.pdf"
 
 #functions
 def combine_pdfs(output_path, documents):
-    pdf_combined = PyPDF.PdfMerger()
+    pdf_combined = PyPDF2.PdfMerger()
 
     for document in documents:
         pdf_combined.append(document) #Add each PDF to the final fusion
     pdf_combined.write(output_path) #Save Combined PDF in output path
 
 #Frontend
-st.image("img/fusion-pdf.png")
-st.header("Combine PDFs Simply and efectively")
+st.set_page_config(page_title="QR Generator", page_icon="📄", layout="centered")
+st.image("img/fusion-pdf.png", use_column_width=True)
+st.title("Combine PDFs Simply and efectively")
 st.subheader("Attach PDFs to combine")
 pdfs_attached = st.file_uploader(label="", accept_multiple_files=True)
 fusion = st.button(label="Combine PDFs")
@@ -24,7 +25,7 @@ if fusion:
         st.Warning("Please, make sure to attach more than one PDF")
     else:
         combine_pdfs(output_pdf, pdfs_attached)
-        st.success("Here it is. You can download the PDF combined.")
+        st.success("Here it is. You can download the combined PDF.")
         with open(output_pdf, "rb") as file:
             pdf_data = file.read()
         st.download_button(label="Download PDF Combined", data=pdf_data, file_name="PDF_Combined.pdf")
